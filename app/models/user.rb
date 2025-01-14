be4b194ace69f31b8979700f670fd8e35b5a495f
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-14 19:01:44 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: "全角（漢字・ひらがな・カタカナ）で入力してください" }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: "全角（漢字・ひらがな・カタカナ）で入力してください" }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "全角カタカナで入力してください" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "全角カタカナで入力してください" }
+    validates :birth_date
+  end
 end
