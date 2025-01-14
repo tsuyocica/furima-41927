955e3ff38cca387a56b419a451a9e87236dbc99c
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-14 17:04:33 +0900

    ユーザー認証の設定、usersテーブルカラム名変更

---
+++ db/migrate/20250114075021_rename_birthday_to_birth_date_in_users.rb
@@ -0,0 +1,5 @@
+class RenameBirthdayToBirthDateInUsers < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :users, :birthday, :birth_date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_14_061231) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_14_075021) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_14_061231) do
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.date "birthday", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
