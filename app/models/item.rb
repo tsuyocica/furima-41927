39034b632fa936fcc8cbee86b294e1a04a4c6ee2
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-15 17:00:39 +0900

    ActiveHashの作成と、アソシエーション＆バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  #ActiveHashのアソシエーション
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+  has_one    :purchase
+
+  #その他のアソシエーション
   belongs_to :user
 
+  #ファイル添付
+  has_one_attached :image
+
+  #バリデーション
   with_options presence: true do
     validates :name
     validates :description
@@ -11,5 +25,13 @@ class Item < ApplicationRecord
     validates :days_to_ship_id
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
-  has_one_attached :image
+
+  #ActiveHashのバリデーション
+  with_options numericality: { other_than: 1, message: "を選択してください" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :days_to_ship
+  end
 end
