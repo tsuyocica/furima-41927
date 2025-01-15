8091f81192942b5b17431f4221dfbcbdcd126f02
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-15 14:11:24 +0900

    テストユーザーのパスワード設定変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { "やまちん" }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { Faker::Internet.password(min_length: 4) + '1a' }
     password_confirmation { password }
     last_name             { "山田" }
     first_name            { "太郎" }
