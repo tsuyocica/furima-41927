279272c60dc4d42061974bbc1292ef1dec4fabeb
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-16 15:51:04 +0900

    商品出品単体テストの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -87,6 +87,17 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
+      it '価格が半角数字でないと出品できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      #ユーザー情報が紐づいていないと出品できない
+      it 'ユーザー情報が紐づいていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
