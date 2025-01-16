714dee0e68bfad37217b44ba32373157a995ef69
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-16 10:26:34 +0900

    商品出品機能単体テスト作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,21 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    category_id { 1 }
-    condition_id { 1 }
-    delivery_fee_id { 1 }
-    region_id { 1 }
-    shipping_time_id { 1 }
-    price { 1 }
-    user { nil }
+    name              { Faker::Commerce.product_name }  # 商品名
+    description       { Faker::Lorem.paragraph }      # 商品の説明
+    category_id       { Faker::Number.between(from: 2, to: 11) }  # カテゴリー
+    condition_id      { Faker::Number.between(from: 2, to: 7) } # 商品の状態
+    delivery_fee_id   { Faker::Number.between(from: 2, to: 3) } # 配送料の負担
+    region_id         { Faker::Number.between(from: 2, to: 48) }  # 発送元の地域
+    shipping_time_id  { Faker::Number.between(from: 2, to: 4) } # 発送までの日数
+    price             { Faker::Number.between(from: 300, to: 9_999_999) } # 価格
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('app', 'assets', 'images', 'test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
