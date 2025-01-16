0d2d246201ce2dc5f130f8083e399b909e84bdd8
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-16 14:27:05 +0900

    エラー時に適切な HTTP ステータスを返す処理を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,8 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new
+      # Rails.logger.debug(@item.errors.full_messages)
+      render :new, status: :unprocessable_entity
     end
   end
 
