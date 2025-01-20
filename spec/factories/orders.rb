47ee483f38d09f2a618ea24f5d7883e9e3fcfe74
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-20 17:00:11 +0900

    orderモデルの単体テストファイル生成

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
   factory :order do
-    user { nil }
-    item { nil }
+    price {3000}
+    token {"tok_abcdefghijk00000000000000000"}
+    postal_code {"123-4567"}
+    region_id {2}
+    city {"東京都"}
+    address_line {"1-1"}
+    building_name {"東京ハイツ"}
+    phone_number {"09012345678"}
+
   end
 end
