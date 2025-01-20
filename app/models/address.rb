560264befe1852e422e8cea6999fbc3ba284853b
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2025-01-20 18:34:28 +0900

    重複したバリデーションを削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,12 +2,4 @@ class Address < ApplicationRecord
   #アソシエーション
   belongs_to :order
 
-  #バリデーション
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
-    validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city
-    validates :address_line
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
-  end
 end
